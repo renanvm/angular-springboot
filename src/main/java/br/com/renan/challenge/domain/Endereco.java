fd3ca5d9ad671ae2d6cd9bb9b278c8127ba55447
Renan Martins <renantins@outlook.com>
2020-08-11 20:35:15 -0300

    Adiciona entidades Pessoa e Endereco

---
+++ src/main/java/br/com/renan/challenge/domain/Endereco.java
@@ -0,0 +1,73 @@
+package br.com.renan.challenge.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+
+    @Id
+    private Long id;
+    private String logradouro;
+    private Integer numero;
+    private String bairro;
+    private String cep;
+    private String estado;
+    private String cidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
---
+++ src/main/java/br/com/renan/challenge/domain/Pessoa.java
@@ -0,0 +1,50 @@
+package br.com.renan.challenge.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Pessoa {
+
+    @Id
+    private Long id;
+    private String nome;
+    private String email;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Endereco> enderecos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+}

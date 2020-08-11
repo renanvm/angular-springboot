d0dc5089a4a097a07de88ec7cc6dac46daea5ce5
Renan Martins <renantins@outlook.com>
2020-08-11 20:42:19 -0300

    Adiciona Repository

---
+++ src/main/java/br/com/renan/challenge/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.renan.challenge.repository;
+
+import br.com.renan.challenge.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}

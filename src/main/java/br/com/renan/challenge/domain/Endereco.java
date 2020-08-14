8d4a8e9873fc8e30d091369c39ca8cffc8c45c0f
Renan Martins <renantins@outlook.com>
2020-08-14 20:59:34 -0300

    Adiciona auto id para entidade Endereco

--- src/main/java/br/com/renan/challenge/domain/Endereco.java
+++ src/main/java/br/com/renan/challenge/domain/Endereco.java
@@ -1,12 +1,15 @@
 package br.com.renan.challenge.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Endereco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String logradouro;
     private Integer numero;
